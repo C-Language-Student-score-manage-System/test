0f2bfcad664b9cef0b7d7e6176e00abe89c1096f
zl-sy <77959540+zl-sy@users.noreply.github.com>
2021-01-30 13:48:29 +0800

    Update struct.c

--- struct.c
+++ struct.c
@@ -4,17 +4,17 @@ struct collage
 {
     int collage_id;
     char collage_name[32];
-};
+}Collage;
 struct sex
 {
     int sex_id;
     char sex_name[32];
-};
+}Sex;
 struct score
 {
     int score_id[4];
     char score_name[4][32];
-};
+}Score;
 typedef struct stu
 {
     long stu_id;
