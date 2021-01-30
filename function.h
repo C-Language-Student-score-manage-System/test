5658119bde1f61f0d0f7a33a248831d84ca34a53
zl-sy <77959540+zl-sy@users.noreply.github.com>
2021-01-30 14:58:11 +0800

    Update function.h

--- function.h
+++ function.h
@@ -32,17 +32,17 @@ void choose_menu()
     {   
         case 1:
         {
-            //
+            Namesorting();
             break;
         }
         case 2:
         {
-            //
+            Averagesorting();
             break;
         }
         case 3:
         {
-            //
+            Collagequerying();
             break;
         }
         case 4:
@@ -52,17 +52,17 @@ void choose_menu()
         }
         case 5:
         {
-            //
+            Revisegrade();
             break;
         }
         case 6:
         {
-            //
+            Namequerying();
             break;
         }
         case 7:
         {
-            //
+            delete_stuscore();
             break;
         }
         case 8:
@@ -72,7 +72,7 @@ void choose_menu()
         }
         case 9:
         {
-            //
+            print_failstu();
             break;
         }
         case 0:
@@ -142,6 +142,168 @@ void log_in()
 //     fclose(fp_p);
 // }
 
+void Namesorting()
+{
+	STU student[500];
+	int n;
+	int i,j;
+	for(i=0;i<n-1;i++)
+		for(j=0;j<n-i-1;j++)
+		{
+			STU temp;
+			if(strcmp(student[j].stu_name,STU[j+1].stu_name)>0)
+			{
+				temp=student[j];
+				student[j]=student[j+1];
+				student[j+1]=temp;
+			}
+		}
+}
+void Averagesorting()
+{
+	STU student[500]; 
+	int n;
+	int i,j;
+	float average[n];
+	for(j=0;j<n;j++)
+	{
+		for(i=0;i<class;i++)
+			average[j]+=student[j].Score.score_id[i];
+		average[j]/=class;
+	}
+	for(i=0;i<n-1;i++)
+		for(j=0;j<n-i-1;j++)
+		{
+			STU temp;
+			if(average[j]>average[j+1])
+			{
+				temp=student[j];
+				student[j]=student[j+1];
+				student[j+1]=temp;
+			}
+		}		
+}
+void Collagequerying()
+{
+	STU student[500];
+	int n;
+	int colnum; 
+	printf("请输入查找学院代号：");
+	scanf("%d",&colnum);
+	int i,j;
+	if(colnum<0||colnum>10)
+		printf("该学院代号为空!\n");
+	else
+	{
+		printf("*********************************************************************************\n");
+		printf("*学号\t姓名\t性别\t学院\t%s\t%s\t%s\t%s\t*\n",student[0].Score.score_name[0],student[0].Score.score_name[1],student[0].Score.score_name[2],student[0].Score.score_name[3]);
+		for(i=0;i<n;i++)
+		{
+			if(student[i].Collage.collage_id==colnum)
+			{
+				printf("%ld\t%s\t%s\t%s",student[i].stu_id,student[i].stu_name,student[i].Sex.sex_name,student[i].Collage.collage_name)
+				for(j=0;j<class;j++)
+				{
+					printf("\t%d\t%d\t%d\t%d\t*\n",student[i].score.score_id[j])
+				}
+			}	
+		}	
+		printf("*********************************************************************************\n");
+	}	
+}
+void Revisegrade()
+{
+	STU student[500];
+	char Stu_name[30];
+	printf("请输入需修改的学生姓名：");
+	scanf("%s",Stu_name);
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(student[i].stu_name,Stu_name)!=0)
+			printf("查无此人!\n");
+		else
+		{
+			printf("*********************************************************************************\n");
+			printf("*原信息:\t学号\t姓名\t性别\t学院\t%s\t%s\t%s\t%s\t*\n",student[0].Score.score_name[0],student[0].Score.score_name[1],student[0].Score.score_name[2],student[0].Score.score_name[3]);
+			printf("*********************************************************************************\n");
+			printf("请输入新成绩:\n");
+			for(j=0;j<class;j++)
+			{
+				printf("%s:",student[i].Score.score_name[j]);
+				scanf("%d\n",&student[i].Score.score_id[j]);	
+			}
+			printf("信息修改成功。\n");
+		}
+	}	
+}
+void Namequerying()
+{
+	STU student[500];
+	char Stu_name[30];
+	printf("请输入查询学生姓名：");
+	scanf("%s",Stu_name);
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(student[i].stu_name,Stu_name)!=0)
+			printf("查无此人!\n");
+		else
+		{
+			printf("*********************************************************************************\n");
+			printf("*学号\t姓名\t性别\t学院\t%s\t%s\t%s\t%s\t*\n",student[0].Score.score_name[0],student[0].Score.score_name[1],student[0].Score.score_name[2],student[0].Score.score_name[3]);
+			printf("%ld\t%s\t%s\t%s",student[i].stu_id,student[i].stu_name,student[i].Sex.sex_name,student[i].Collage.collage_name)
+			for(j=0;j<class;j++)
+			{
+				printf("\t%d\t%d\t%d\t%d\t*\n",student[i].score.score_id[j])
+			}
+			printf("*********************************************************************************\n");
 
+		}
+	}
+}
+
+void delete_stuscore()
+{
+	STU student[500];
+	char Stu_name[30];
+	printf("请输入需删除学生成绩的姓名：");
+	scanf("%s",Stu_name);
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		if(strcmp(student[i].stu_name,Stu_name)!=0)
+			printf("查无此人!\n");
+		else
+		{
+			for(j=0;j<class;j++)
+				student[i].Score.score_id[j]=0;
+		}
+	} 
+} 
+
+void print_failstu()
+{
+	STU student[500];
+	int i,j;
+	printf("****************挂科学生及科目名单*************");
+	for(i=0;i<n;i++)
+	{
+		int flag=0;
+		for(j=0;j<class;j++)
+		{
+			if(student[i].score.score_id[j]<60)
+			{
+				if(flag==0)
+					printf("%s\t%s\t",student[i].stu_name,student[i].Score.score_name[j]);
+				else
+					printf("%s\t",student[i].Score.score_name[j]);
+				flag=1;			
+			}
+		}
+		printf("\n");
+	}
+	printf("**********************************************");
+}
 
 
